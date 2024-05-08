f25d3e4aef7bde4d565d96a191181ea1926e41bc
MaximTs1 <129722965+MaximTs1@users.noreply.github.com>
2024-05-08 19:00:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,9 +17,7 @@ const port = process.env.PORT || 3001;
 
 app.use(logger);
 
-app.use(express.json({ limit: "50mb" }));
-
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
 
 app.use(cors({
     origin: true,
